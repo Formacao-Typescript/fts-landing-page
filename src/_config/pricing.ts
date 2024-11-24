2377be4b2a08ad247aff17cf7f4c8e8de5887a5b
Lucas Santos <lhs.santoss@gmail.com>
2024-11-25 00:20:35 +0100

    fix: pricing

--- src/_config/pricing.ts
+++ src/_config/pricing.ts
@@ -1,13 +1,13 @@
 const pricing = {
-  monthly: parseFloat(import.meta.env.PUBLIC_MONTHLY_PRICE),
-  yearly: parseFloat(import.meta.env.PUBLIC_YEARLY_PRICE),
+    monthly: Number(import.meta.env.PUBLIC_MONTHLY_PRICE),
+    yearly: Number(import.meta.env.PUBLIC_YEARLY_PRICE),
 };
 
 export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }).format(price);
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(price);
 };
 
 export default pricing;
