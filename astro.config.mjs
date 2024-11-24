898f79a939b6eaac1d58e183a937f31f75ada013
Lucas Santos <lhs.santoss@gmail.com>
2024-11-25 00:02:53 +0100

    fix: base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://formacaots.com.br'
+    site: 'https://formacaots.com.br',
+    base: ''
 });
